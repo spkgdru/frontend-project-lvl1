b45afe9550844a01216d0e483a88c33e54e0cf16
spkgdru <37667046+spkgdru@users.noreply.github.com>
2019-12-28 13:49:41 +0200

    Update brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,21 +1,5 @@
 #!/usr/bin/env node
 
-import game from '..';
+import init from '..';
 
-const greeting = 'Find the greatest common divisor of given numbers.';
-
-const quest = () => {
-  const num1 = Math.floor(Math.random() * 100);
-  const num2 = Math.floor(Math.random() * 100);
-  const [bigNum, smallNum] = num1 >= num2 ? [num1, num2] : [num2, num1];
-  const gcd = (big, small) => {
-    if (big % small === 0) return small;
-    return gcd(small, big % small);
-  };
-  return {
-    question: `${num1} ${num2}`,
-    answer: `${gcd(bigNum, smallNum)}`,
-  };
-};
-
-game(greeting, quest);
+init(brainGcd);
