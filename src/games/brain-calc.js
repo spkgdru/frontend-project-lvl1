dfc65c56dc6468fb047c6f9fd6deb03aba2e275b
spkgdru <37667046+spkgdru@users.noreply.github.com>
2019-12-28 13:47:33 +0200

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,25 +1,5 @@
 #!/usr/bin/env node
 
-import game from '..';
+import init from '..';
 
-const greeting = 'What is the result of the expression?';
-
-const quest = () => {
-  const num1 = Math.floor(Math.random() * 100);
-  const num2 = Math.floor(Math.random() * 100);
-  const sign = Math.floor(Math.random() * 2);
-  const question = {
-    0: `${num1}+${num2}`,
-    1: `${num1}*${num2}`,
-  };
-  const answer = {
-    0: num1 + num2,
-    1: num1 * num2,
-  };
-  return {
-    question: question[sign],
-    answer: `${answer[sign]}`,
-  };
-};
-
-game(greeting, quest);
+init(brainCalc);
