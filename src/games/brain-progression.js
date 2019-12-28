44a02d1b13fbb0ac6e5c128e1b32a68134cc6b54
spkgdru <37667046+spkgdru@users.noreply.github.com>
2019-12-28 13:50:55 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,26 +1,5 @@
 #!/usr/bin/env node
 
-import game from '..';
+import init from '..';
 
-const greeting = 'What number is missing in the progression?';
-
-const quest = () => {
-  const progressStartValue = Math.floor(Math.random() * 30);
-  const progressStep = Math.floor(Math.random() * 14) + 1;
-  const position = Math.floor(Math.random() * 10);
-  const progressLength = 10;
-
-  const progress = (element, step, amountOfNumbers, unknownPosition) => {
-    if (amountOfNumbers === 0) return '';
-    if (unknownPosition === 0) return `.. ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-    return `${element} ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-  };
-  const unknownElement = progressStartValue + progressStep * position;
-
-  return {
-    question: progress(progressStartValue, progressStep, progressLength, position),
-    answer: `${unknownElement}`,
-  };
-};
-
-game(greeting, quest);
+init(brainProgression);
