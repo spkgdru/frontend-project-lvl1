9b305a6cfdf89bcc43de53357f39d4b03ab6002c
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 01:03:30 +0200

    Delete prime.js

--- src/prime.js
+++
@@ -1,9 +0,0 @@
-const prime = (num) => {
-  const check = (div) => {
-    if (div === 1) return true;
-    return num % div === 0 ? false : check(div - 1);
-  };
-  return check(Math.floor(Math.sqrt(num)));
-};
-
-export default prime;
