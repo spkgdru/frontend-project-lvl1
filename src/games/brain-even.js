1b6a0ccae762da009f698265e4b2b4c3f7cb0784
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 00:07:16 +0200

    Update brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,13 +1,15 @@
 import game from '..';
 import random from '../random';
-import even from '../even';
 
-export default () => {
-  const greeting = 'Answer "yes" if the number is even, otherwise answer "no"';
-  const quest = () => {
+const even = (num) => num % 2 === 0;
+const greeting = 'Answer "yes" if the number is even, otherwise answer "no"';
+const quest = () => {
     const question = random(1, 100);
     const answer = even(question) ? 'yes' : 'no';
     return { question, answer };
   };
+
+
+export default () => {
   game(greeting, quest);
 };
