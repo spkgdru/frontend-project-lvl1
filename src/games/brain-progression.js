d66b68b2e30740527c6b45536a3a7e17ff7bc7b2
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 01:08:32 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,23 +1,25 @@
 import game from '..';
 import random from '../random';
 
-export default () => {
-  const greeting = 'What number is missing in the progression?';
-  const quest = () => {
-    const progressStartValue = random(1, 30);
-    const progressStep = random(2, 15);
-    const position = random(0, 9);
-    const progressLength = 10;
-    const progress = (element, step, amountOfNumbers, unknownPosition) => {
-      if (amountOfNumbers === 0) return '';
-      if (unknownPosition === 0) return `.. ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-      return `${element} ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-    };
-    const unknownElement = progressStartValue + progressStep * position;
-    return {
-      question: progress(progressStartValue, progressStep, progressLength, position),
-      answer: `${unknownElement}`,
-    };
+const progressLength = 10;
+
+const greeting = 'What number is missing in the progression?';
+const play = () => {
+  const progressStartValue = random(1, 30);
+  const progressStep = random(2, 15);
+  const position = random(0, 9);
+  const progress = (element, step, amountOfNumbers, unknownPosition) => {
+    if (amountOfNumbers === 0) return '';
+    if (unknownPosition === 0) return `.. ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
+    return `${element} ${progress(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
   };
-  game(greeting, quest);
+  const unknownElement = progressStartValue + progressStep * position;
+  return {
+    question: progress(progressStartValue, progressStep, progressLength, position),
+    answer: unknownElement.toString(),
+  };
+};
+
+export default () => {
+  game(greeting, play);
 };
