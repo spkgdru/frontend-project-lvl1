6713e534849c659d4b4041159e31fd98c6626e46
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 00:37:08 +0200

    Update brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,16 +2,17 @@ import game from '..';
 import random from '../random';
 import gcd from '../gcd';
 
-export default () => {
-  const greeting = 'Find the greatest common divisor of given numbers.';
-  const quest = () => {
-    const num1 = random(1, 100);
-    const num2 = random(1, 100);
-    const [bigNum, smallNum] = num1 >= num2 ? [num1, num2] : [num2, num1];
-    return {
-      question: `${num1} ${num2}`,
-      answer: `${gcd(bigNum, smallNum)}`,
-    };
+const greeting = 'Find the greatest common divisor of given numbers.';
+const quest = () => {
+  const num1 = random(1, 100);
+  const num2 = random(1, 100);
+  const [bigNum, smallNum] = num1 >= num2 ? [num1, num2] : [num2, num1];
+  return {
+    question: `${num1} ${num2}`,
+    answer: `${gcd(bigNum, smallNum)}`,
   };
+};
+
+export default () => {
   game(greeting, quest);
 };
