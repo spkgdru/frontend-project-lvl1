1e1ba465c3b615df470c369d70fc949a0f2c0b54
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 00:35:20 +0200

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,25 +1,36 @@
 import game from '..';
 import random from '../random';
 
-export default () => {
-  const greeting = 'What is the result of the expression?';
-  const quest = () => {
-    const amountOfOperations = 2;
-    const num1 = random(1, 100);
-    const num2 = random(1, 100);
-    const sign = random(1, amountOfOperations);
-    const question = {
-      1: `${num1}+${num2}`,
-      2: `${num1}*${num2}`,
-    };
-    const answer = {
-      1: num1 + num2,
-      2: num1 * num2,
-    };
-    return {
-      question: question[sign],
-      answer: `${answer[sign]}`,
-    };
+const operations = [
+  {question: `${num1}+${num2}`, answer: num1 + num2},
+  {question: `${num1}*${num2}`, answer: num1 & num2}
+];
+
+const randomOperation = (num1, num2) => {
+  const case = random(0, operations.length -1);
+  return {
+    question: operations[case].
+}
+
+const greeting = 'What is the result of the expression?';
+const quest = () => {
+  const num1 = random(1, 100);
+  const num2 = random(1, 100);
+  const sign = random(1, amountOfOperations);
+  const question = {
+    1: `${num1}+${num2}`,
+    2: `${num1}*${num2}`,
   };
+  const answer = {
+    1: num1 + num2,
+    2: num1 * num2,
+  };
+  return {
+    question: question[sign],
+    answer: `${answer[sign]}`,
+  };
+};
+
+export default () => {
   game(greeting, quest);
 };
