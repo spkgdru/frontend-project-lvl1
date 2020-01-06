92954c8214ece95f93869b89cfc82afa6bf1d16b
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 01:05:07 +0200

    Update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,13 +1,21 @@
 import game from '..';
 import random from '../random';
-import prime from '../prime';
 
-export default () => {
-  const greeting = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const prime = (num) => {
+  const check = (div) => {
+    if (div === 1) return true;
+    return num % div === 0 ? false : check(div - 1);
+  };
+  return check(Math.floor(Math.sqrt(num)));
+};
+
+const greeting = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const quest = () => {
     const question = random(1, 3000);
     const answer = prime(question) ? 'yes' : 'no';
     return { question, answer };
   };
+
+export default () => {
   game(greeting, quest);
 };
