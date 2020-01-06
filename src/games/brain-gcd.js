879c80533b80239ec7dcf55892f3e216b9ec4589
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-07 01:02:28 +0200

    Update brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,6 +1,10 @@
 import game from '..';
 import random from '../random';
-import gcd from '../gcd';
+
+const gcd = (num1, num2) => {
+  if (num1 === num2) return num1;
+  return gcd(num2, Math.abs(num2 - num1));
+};
 
 const greeting = 'Find the greatest common divisor of given numbers.';
 const quest = () => {
