92b43bd42a9c8ab5fa385279aaf0e33bda0325fa
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-12 11:08:37 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,12 +2,12 @@ import game from '..';
 import getRandom from '../random';
 
 const progressionStartValue = getRandom(1, 30);
-const progressionStep = getRandom(2, 15);
 const progressionLength = 10;
 
 const greeting = 'What number is missing in the progression?';
 const createGameData = () => {
   const emptyPosition = getRandom(0, progressionLength - 1);
+  const progressionStep = getRandom(2, 15);
   const createProgression = (element, step, amountOfNumbers, unknownPosition) => {
     if (amountOfNumbers === 0) return '';
     if (unknownPosition === 0) return `.. ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
