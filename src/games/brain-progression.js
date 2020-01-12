714e4b554a35c55f5ff3d259bb3f921ee3ecf665
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-12 14:44:51 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -14,13 +14,13 @@ const createGameData = () => {
     if (amountOfNumbers === 0) return '';
     if (unknownPosition === 0) return `.. ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
     return `${element} ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-    };
+    }
   const unknownElement = seriesStartValue + seriesStep * emptyPosition;
   return {
     question: createProgression(seriesStartValue, seriesStep, seriesLength, emptyPosition),
     answer: unknownElement.toString(),
   };
-};
+}
 
 export default () => {
   game(greeting, createGameData);
