4b46ed5f4fcc91f4817197a55faa3154b2ba996a
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-12 11:20:25 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,21 +1,23 @@
 import game from '..';
 import getRandom from '../random';
 
-const progressionStartValue = getRandom(1, 30);
-const progressionLength = 10;
+const seriesStartValue = getRandom(1, 30);
+const seriesLength = 10;
+const seriesMinStep = 2;
+const seriesMaxStep = 15;
 
 const greeting = 'What number is missing in the progression?';
 const createGameData = () => {
-  const emptyPosition = getRandom(0, progressionLength - 1);
-  const progressionStep = getRandom(2, 15);
+  const emptyPosition = getRandom(0, seriesLength - 1);
+  const seriesStep = getRandom(seriesMinStep, seriesMaxStep);
   const createProgression = (element, step, amountOfNumbers, unknownPosition) => {
     if (amountOfNumbers === 0) return '';
     if (unknownPosition === 0) return `.. ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
     return `${element} ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
     };
-  const unknownElement = progressionStartValue + progressionStep * emptyPosition;
+  const unknownElement = seriesStartValue + seriesStep * emptyPosition;
   return {
-    question: createProgression(progressionStartValue, progressionStep, progressionLength, emptyPosition),
+    question: createProgression(seriesStartValue, seriesStep, seriesLength, emptyPosition),
     answer: unknownElement.toString(),
   };
 };
