eb9cff93657de89ff77935e3b9256ce477848def
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-12 10:29:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import readlineSync from 'readline-sync';
 
 const answersToWin = 3;
 
-const game = (greeting, quest) => {
+const game = (greeting, gameData) => {
   console.log('Welcome to the Brain Games!');
   console.log(greeting || '');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
-  if (!quest) return;
+  if (!gameData) return;
   const attempt = (result = 0) => {
     if (result === answersToWin) {
       console.log(`Congratulations, ${userName}!`);
       return;
     }
-    const current = quest();
-    console.log(`Question: ${current.question}`);
+    const currentData = gameData();
+    console.log(`Question: ${currentData.question}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === current.answer) {
+    if (answer === currentData.answer) {
       console.log('Correct!');
       attempt(result + 1);
       return;
     }
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${current.answer}'.\nLet's try again, ${userName}!`);
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${currentData.answer}'.\nLet's try again, ${userName}!`);
   };
   attempt();
 };
