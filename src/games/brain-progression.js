df90e5c874f90aca550d9838e79f4042a02c6611
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-01-14 15:02:07 +0200

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,6 +1,12 @@
 import game from '..';
 import getRandom from '../random';
 
+const createProgression = (element, step, amountOfNumbers, unknownPosition) => {
+  if (amountOfNumbers === 0) return '';
+  if (unknownPosition === 0) return `.. ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
+  return `${element} ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
+};
+
 const seriesLength = 10;
 const seriesMinStep = 2;
 const seriesMaxStep = 15;
@@ -11,11 +17,6 @@ const createGameData = () => {
   const emptyPosition = getRandom(0, seriesLength - 1);
   const seriesStep = getRandom(seriesMinStep, seriesMaxStep);
   const answer = (seriesStartValue + seriesStep * emptyPosition).toString();
-  const createProgression = (element, step, amountOfNumbers, unknownPosition) => {
-    if (amountOfNumbers === 0) return '';
-    if (unknownPosition === 0) return `.. ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-    return `${element} ${createProgression(element + step, step, amountOfNumbers - 1, unknownPosition - 1)} `;
-  };
   const question = createProgression(seriesStartValue, seriesStep, seriesLength, emptyPosition);
   return { question, answer };
 };
